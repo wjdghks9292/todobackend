cf02644897317087f8b4a8614d8d7d35f69ddfad
wjdghks9494 <wjdghks9292@naver.com>
2024-01-29 17:34:55 +0900

    firstgit

---
+++ todoproject/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from todoapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('todo', views.TodoView.as_view()),
+]
